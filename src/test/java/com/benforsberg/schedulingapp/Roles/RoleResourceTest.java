62a628399d9b6ca2539dbf2d4d21de0b671c6ed2
Ben Forsberg <benj.forsberg@gmail.com>
2021-09-23 11:59:45 -0700

    Added JUnit Test Class, more coming soon.

---
+++ src/test/java/com/benforsberg/schedulingapp/Roles/RoleResourceTest.java
@@ -0,0 +1,34 @@
+package com.benforsberg.schedulingapp.Roles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleResourceTest {
+
+    @Test
+    void retrieveAllRoles() {
+        assertTrue(true);
+        System.out.println("Success! Passed!");
+    }
+
+    @Test
+    void retrieverole() {
+        assertTrue(true);
+    }
+
+    @Test
+    void deleteRole() {
+        assertTrue(true);
+    }
+
+    @Test
+    void createRole() {
+        assertTrue(true);
+    }
+
+    @Test
+    void updateRole() {
+        assertTrue(true);
+    }
+}
